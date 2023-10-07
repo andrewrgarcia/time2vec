5d8dd7c76ad4962064cdfd44e4db884a35e6d353
Andrew Garcia <garcia.gtr@gmail.com>
2023-10-07 11:56:04 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Keras Time2Vec
+
+An intuitive, custom Keras Layer for Time2Vec Transformation.
+
+Keras implementation of Non-local blocks from [[1]](https://arxiv.org/abs/1711.07971).
+
+
+# Concept
+
+Time2Vec offers a versatile representation of time with three fundamental properties. It encapsulates scalar notion of time $\tau$,  in $\mathbf{t2v}(\tau)$,
+a vector of size k + 1. This transformation is defined as follows[[1]]:
+
+
+```math
+\mathbf{t2v}(\tau)[i] = 
+    \begin{cases}
+        \omega_i \tau + \phi_i \if i = 0.\\
+        \mathcal{F}(\omega_i \tau + \phi_i) \if  \leq i \leq k
+    \end{cases}
+```
+
+
+
+The below image shows a random walk and with its $\mathbf{t2v}$ (with a $k$ of k=20):
+<center><img src="https://github.com/andrewrgarcia/keras-time2vec/blob/master/images/rep.png?raw=true" width=50% ></center>
+
+
+
+# Usage Templates
+
+The script `time2vec.py` contains the `Time2Vec` instance which takes a single or a group of time series and concatenates the above $mathbf{t2v}$ tensor to it.
+
+```python
+from time2vec import Time2Vec
+
+k = 20
+time_series = Time2Vec(num_frequency=k)(time_series)
+print(time_series)
+...
+```
+
+
+
+1. Seyed Mehran Kazemi, Rishab Goel, Sepehr Eghbali, Janahan Ramanan, Jaspreet Sahota, Sanjay Thakur, Stella Wu, Cathal Smyth, Pascal Poupart, Marcus Brubaker. "Time2Vec: Learning a Vector Representation of Time." arXiv:1907.05321 [cs.LG], 11 Jul 2019. [Link](https://arxiv.org/abs/1907.05321)
---
+++ time2vec.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+class Time2Vec(layers.Layer):
+    def __init__(self, num_frequency, **kwargs):
+        """
+        Custom Keras Layer for Time2Vec Transformation.
+
+        Parameters
+        -------------
+        num_frequency: int
+            The number of periodic components to model.
+        """
+        super(Time2Vec, self).__init__(**kwargs)
+        self.num_frequency = num_frequency
+
+    def build(self, input_shape):
+        super(Time2Vec, self).build(input_shape)
+
+        self.trend_weight = self.add_weight(
+            name="trend_weight",
+            shape=(1,),
+            initializer="uniform",
+            trainable=True,
+        )
+        self.trend_bias = self.add_weight(
+            name="trend_bias",
+            shape=(1,),
+            initializer="uniform",
+            trainable=True,
+        )
+
+        self.periodic_weight = self.add_weight(
+            name="periodic_weight",
+            shape=(1, self.num_frequency),
+            initializer="uniform",
+            trainable=True,
+        )
+        self.periodic_bias = self.add_weight(
+            name="periodic_bias",
+            shape=(input_shape[0], self.num_frequency),
+            initializer="uniform",
+            trainable=True,
+        )
+
+    def call(self, x):
+        t = tf.range(tf.shape(x)[0], dtype=tf.float32)
+        t = tf.reshape(t, (-1, 1))
+
+        # Trend component
+        trend_component = self.trend_weight * t + self.trend_bias
+
+        # Periodic component
+        periodic_component = tf.sin(
+            tf.matmul(t, self.periodic_weight) + self.periodic_bias
+        )
+
+        return tf.concat([x, trend_component, periodic_component], axis=-1)
+
+    def compute_output_shape(self, input_shape):
+        """
+        Computes the output shape of the layer.
+
+        Parameters
+        ----------
+        input_shape: tuple
+            The shape of the input data.
+        """
+        return input_shape[:-1] + (input_shape[-1] + self.num_frequency * 2,)
