66118c26d099a800a6a04535b7a6ea12c34aac93
Andrew Garcia <garcia.gtr@gmail.com>
2024-06-25 11:51:15 -0500

    multivariate tensor and internal variable , time splitter (tensor must have time information in last vector of said tensor)

--- time2vec.py
+++ time2vec.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 
+@tf.keras.utils.register_keras_serializable()
 class Time2Vec(layers.Layer):
-    def __init__(self, num_frequency, **kwargs):
+    def __init__(self, num_frequency, num_vars, **kwargs):
         """
         Custom Keras Layer for Time2Vec Transformation.
 
@@ -14,6 +15,7 @@ class Time2Vec(layers.Layer):
         """
         super(Time2Vec, self).__init__(**kwargs)
         self.num_frequency = num_frequency
+        self.num_vars = num_vars
 
     def build(self, input_shape):
         super(Time2Vec, self).build(input_shape)
@@ -33,31 +35,37 @@ class Time2Vec(layers.Layer):
 
         self.periodic_weight = self.add_weight(
             name="periodic_weight",
-            shape=(1, self.num_frequency),
+            shape=(input_shape[-1] - self.num_vars + 1, self.num_frequency),
             initializer="uniform",
             trainable=True,
         )
         self.periodic_bias = self.add_weight(
             name="periodic_bias",
-            shape=(input_shape[0], self.num_frequency),
+            shape=(input_shape[1], self.num_frequency),
             initializer="uniform",
             trainable=True,
         )
 
-    def call(self, x):
-        t = tf.range(tf.shape(x)[0], dtype=tf.float32)
-        t = tf.reshape(t, (-1, 1))
+    def call(self, inputs):
+        # Split inputs into x and t
+        x = inputs[:, :, :self.num_vars-1]
+        t = inputs[:, :, self.num_vars-1:]
+        print("\nt",t.shape)
 
         # Trend component
         trend_component = self.trend_weight * t + self.trend_bias
 
         # Periodic component
-        periodic_component = tf.sin(
-            tf.matmul(t, self.periodic_weight) + self.periodic_bias
-        )
+        periodic_component = tf.sin(tf.matmul(t, self.periodic_weight) + self.periodic_bias)
 
-        return tf.concat([x, trend_component, periodic_component], axis=-1)
+        # Concatenate trend and periodic components
+        t_encoded = tf.concat([trend_component, periodic_component], axis=-1)
 
+        # Concatenate x and t_encoded
+        output = tf.concat([x, t_encoded], axis=-1)
+        
+        return output
+    
     def obtain_output_shape(self, input_shape):
         """
         Obtain the output shape of the layer.
@@ -68,5 +76,5 @@ class Time2Vec(layers.Layer):
             The shape of the input data.
         """
         # Sum of trend (1) and periodic components
-        output_feature_dim = input_shape[-1] + self.num_frequency       
+        output_feature_dim = (input_shape[-1] - self.num_vars + 1) + self.num_frequency + self.num_vars - 1
         return (input_shape[0], input_shape[1], output_feature_dim)
